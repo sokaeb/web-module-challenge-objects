97ad689fbb05297886c7d12f521e49f6d56f3d27
Kae Benton <sonja.k.benton@gmail.com>
2020-07-17 10:14:28 -0400

    Worked on the last stretch goal.

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 ///////////////Menu Items (MVP)///////////////////
 
-const latte = {name: "Cafe Latte", price: 4, category: "Drinks"};
-const burger = {name: "Burger", price: 18, category: "Lunch"};
-const breakfastBurrito = {name: "Breakfast Burrito", price: 16, category:"Breakfast"};
+// const latte = {name: "Cafe Latte", price: 4, category: "Drinks"};
+// const burger = {name: "Burger", price: 18, category: "Lunch"};
+// const breakfastBurrito = {name: "Breakfast Burrito", price: 16, category:"Breakfast"};
 
 /* Task 1a: write a function to return more menu items with the same format as the items above. */
 
@@ -42,16 +42,16 @@ For example, burger.discount("teacher") would return 13.5 and burger.discount("p
 
 ///////////////Reviews (MVP)///////////////////
 
-const reviews = [
-                  {name: "Daniela", rating: 5, feedback:"Beautiful atmosphere and wonderful vegan options!"},
-                  {name: "Jack", rating: 3, feedback:"A little too hipster for my taste, but the burger was decent, if overpriced"},
-                  {name: "Miranda", rating: 4, feedback:"fun trivia and cool vibes"},
-                  {name: "Wen", rating: 4.5, feedback:"I don't leave my house often, but when I do, it's for this place. Highly reccomend."},
-                  {name: "Brett", rating: 3, feedback: "great selection of snacks and a nice cafe area to get work done during the day."},
-                  {name: "Julius", rating: 2, feedback: "I was largely unimpressed by this venue. Nothing special on the menu and too expensive. The atmosphere is polarizing, and not for me, but I think some would like it." },
-                  {name:"Lauren", rating: 4, feedback: "Absolutely love that they have karaoke Fridays! Food and drink selection is okay."},
-                  {name:"Reyna", rating: 3.5, feedback: ""},
-]
+// const reviews = [
+//                   {name: "Daniela", rating: 5, feedback:"Beautiful atmosphere and wonderful vegan options!"},
+//                   {name: "Jack", rating: 3, feedback:"A little too hipster for my taste, but the burger was decent, if overpriced"},
+//                   {name: "Miranda", rating: 4, feedback:"fun trivia and cool vibes"},
+//                   {name: "Wen", rating: 4.5, feedback:"I don't leave my house often, but when I do, it's for this place. Highly reccomend."},
+//                   {name: "Brett", rating: 3, feedback: "great selection of snacks and a nice cafe area to get work done during the day."},
+//                   {name: "Julius", rating: 2, feedback: "I was largely unimpressed by this venue. Nothing special on the menu and too expensive. The atmosphere is polarizing, and not for me, but I think some would like it." },
+//                   {name:"Lauren", rating: 4, feedback: "Absolutely love that they have karaoke Fridays! Food and drink selection is okay."},
+//                   {name:"Reyna", rating: 3.5, feedback: ""},
+// ]
 
 /* Task 3: Console.log just Julius' feedback */
 
@@ -175,12 +175,16 @@ The returned object should have the following characteristics:
 
 
 function carMaker(num) {
-  return {'odometer': num, 'drive': function(dist){ 
-    odometer = num*dist
-      return odometer; 
-      }   
-    }
+  return {
+      'odometer': num, 
+      'drive': function(dist){ 
+      this.odometer += dist;
+      return this.odometer;
+      } 
+        }
   }
   console.log(carMaker(5));
+  console.log(carMaker(5).drive(2));
+  
 
   
